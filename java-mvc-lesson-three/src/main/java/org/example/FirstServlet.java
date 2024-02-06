9ba5e84c4c5055df7cff54f20e5bc40dde1c608b
Vadim Shvedov <shvedovva@gmail.com>
2024-02-06 05:15:32 +0300

    Add lesson three and config

---
+++ java-mvc-lesson-three/src/main/java/org/example/FirstServlet.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/TServlet")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h2>Inside servlet!</h2>");
+
+    }
+}
